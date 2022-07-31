16258bca5813e05cdfe8afaaa40fe6b687d47ae3
Daniel Gogo <110302238+Daniel-Gogo@users.noreply.github.com>
2022-07-31 15:51:31 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function getHistory(){
+    document.getElementById("historyvalue").innerHTML;
+}
+function printHistory(num){
+    document.getElementById("historyvalue").innerHTML;
+}
+function getOutput(){
+    return document.getElementById("outputvalue").innerText;
+}
+function printOutput(num){
+    document.getElementById("outputvalue").innerText=num;
+}
